fab7e7b1b89d2d9934cdda7f4138b09804d649a3
RuanCunha <ruanvcunha@hotmail.com>
2022-11-23 18:26:42 -0300

    feat: 🎸 Implement js and open/close navbar

---
+++ javascript/script.js
@@ -0,0 +1,7 @@
+function openNav() {
+  document.getElementById("navbar").style.transform = "translateX(0)";
+}
+
+function closeNav() {
+  document.getElementById("navbar").style.transform = "translateX(-100%)";
+}
